208cf1fcb94ec2a33fd6628736bce74237b3a3d8
Atul Anand <atulcleaver@gmail.com>
2022-11-21 05:15:49 +0530

    Cleaned out comments

--- src/App.js
+++ src/App.js
@@ -8,39 +8,6 @@ import Music from './components/Music.jsx';
 function App() {
   return (
     <div className="App">
-      {/* 
-      ✅ Navbar
-        - Logo
-        - Heading
-      ✅ Background Animation
-      Pomodoro Timer
-        ✅ Timer
-        ✅ Start/Stop Button
-        ✅ Focus/Short Break/Long Break Buttons
-      ✅ Tips
-        ✅ Tip 1 for Focus
-        ✅ Tip 2 for Short Break
-        ✅ Tip 3 for Long Break
-      Todo List
-        - Connect with Google Calendar
-        ✅ Task List
-        ✅ Add Task Button
-        ✅ Delete Task Button
-      ✅ Footer
-        - Github Link
-        - LinkedIn Link
-        - Email Link
-        - Twitter Link
-        - Instagram Link
-        - Website Link
-      ✅ Music Controls
-        Music Radio
-          - Nature Sounds
-          - Lofi Music
-        Music Controls
-          - Mute/Unmute Button
-          - Volume Slider
-      */}
       <Navbar />
       <Music />
       <Pomodoro />
